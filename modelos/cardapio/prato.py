eff11024391e2d1145638fcef626117e6c6b328a
Tiago dos Santos <tiago2904santos@gmail.com>
2025-11-04 17:51:18 -0300

    Polimorfismo e método abstrato

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -4,3 +4,6 @@ class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao):
         super().__init__(nome, preco)
         self._descricao = descricao
+
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,6 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
+
 
 class Restaurante:
     restaurantes = []
@@ -8,6 +10,7 @@ class Restaurante:
         self._categoria = categoria.title()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -40,3 +43,20 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
+
+
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+
+
+    @property
+    def exibir_cardapio(self):
+        print(f"Cardapio do restaurante {self._nome}\n")
+        for i, item in enumerate(self._cardapio, start=1):
+            if hasattr(item, '_descricao'):
+                mensagem_prato = f"{i}. {item._nome} | R$ {item._preco} | Descrição: {item._descricao}\n"
+                print(mensagem_prato)
+            elif hasattr(item, '_tamanho'):
+                mensagem_bebida = f"{i}. {item._nome} | R$ {item._preco} | Tamanho: {item._tamanho}\n"
+                print(mensagem_bebida)
