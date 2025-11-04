12ac9131d03dfca8af47d11970b04f533b8f3e8f
Tiago dos Santos <tiago2904santos@gmail.com>
2025-11-04 16:52:02 -0300

    herança

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante("praça", "gourmet")
-restaurante_praca.receber_avaliacao("Tiago", 3)
-restaurante_praca.receber_avaliacao("Julia", 9)
+bebida_suco = Bebida("Suco de Melancia", 5, "Grande")
+prato_paozinho = Prato("Pãozinho", 2, "O melhor pão da cidade")
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
 
 if __name__ == "__main__":
     main()
